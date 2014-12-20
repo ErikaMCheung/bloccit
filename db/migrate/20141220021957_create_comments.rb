1f3e3ce3e75552b3b93e73d98d11a0a0eaf51918
Erika Cheung <erika.m.cheung@gmail.com>
2014-12-19 18:36:25 -0800

    Created Post and Comment models with relationships

---
+++ db/migrate/20141220021957_create_comments.rb
@@ -0,0 +1,10 @@
+rmclass CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.text :body
+      t.references :post, index: true
+
+      t.timestamps
+    end
+  end
+end
