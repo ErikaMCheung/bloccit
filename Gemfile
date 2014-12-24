c21a893bcc8ffab933ac0f8c933baa7aee4ba7c2
Erika Cheung <erika.m.cheung@gmail.com>
2014-12-24 10:58:59 -0800

    Changes to Seed data creating fake posts and ccomments

--- Gemfile
+++ Gemfile
@@ -5,13 +5,14 @@ group :production do
   gem 'rails_12factor'
 end
 
+
 group :development do 
   gem 'sqlite3'
 end
-
+#Develop data automatically
+gem 'faker'
 #Boostrap CSS framework 
 gem 'bootstrap-sass', '~> 3.1.1'
-
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.8'
 # Use SCSS for stylesheets
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+
+# Create Comments
+ 50.times do  
+  Post.create!(
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph
+    )
+end
+posts = Post.all
+
+# Create Comments
+100.times do 
+  Comment.create!(
+  post: posts.sample,
+  body: Faker::Lorem.paragraph
+  )
+end
+
+puts "Seed finished"
+puts "#{Post.count} post created"
+puts "#{Comment.count} comments created"
+
+
+
