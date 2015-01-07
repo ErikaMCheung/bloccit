c07585fd111127a80cfcb161b7544f4611830fb5
Erika Cheung <erika.m.cheung@gmail.com>
2015-01-06 18:48:16 -0800

    Creating Topics with associations and authroizations.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,24 @@
 # This is the controller for displaying the posts.
 class PostsController < ApplicationController
   def index
+    @topic = Topic.find(params[:topic_id])
     @posts = Post.all
   end
 
   def show
     @post = Post.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
       authorize @post
   end
 
 # This is the controller for creating posts.
   def create
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new(params.require(:post).permit(:title, :body))
     @post = current_user.posts.build(params.require(:post).permit(:title, :body))
     @post = Post.new(params.require(:post).permit(:title, :body))
@@ -22,7 +26,7 @@ class PostsController < ApplicationController
     
     if @post.save
       flash[:notice] = "Post was saved."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
     end
@@ -30,14 +34,16 @@ class PostsController < ApplicationController
 
 # This is the controller for editing the posts on the main page.
   def edit
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
   end
 
   def update
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     if @post.update_attributes(params.require(:post).permit(:title, :body))
       flash[:notice] = "Post was updated."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :edit
